c917c708703e408f245913cc7cb682388537b6c4
sadiq <sadiqhd@gmail.com>
2024-11-04 02:08:52 +0300

    ng build

--- src/app/seller/seller.module.ts
+++ src/app/seller/seller.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SellerRoutingModule } from './seller-routing.module';
-import { HeaderSellerComponent } from './header-seller/header-seller.component';
 import { ExternalSellerHomeComponent } from './external-seller-home/external-seller-home.component';
 
 
 @NgModule({
   declarations: [
-    HeaderSellerComponent,
     ExternalSellerHomeComponent,
     
   ],
